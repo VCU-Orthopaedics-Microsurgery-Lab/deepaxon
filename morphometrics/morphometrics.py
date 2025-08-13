ee85dd4f669177fe260c34e9da0912753e4d6b03
Marc Mazur <marc_mazur@mac.com>
2025-08-13 12:11:07 -0400

    On branch v2 Your branch is up to date with 'origin/v2'.
    Changes to be committed:
    	modified:   morphometrics/__main__.py
    	new file:   morphometrics/__pycache__/__main__.cpython-312.pyc
    	modified:   morphometrics/__pycache__/__main__.cpython-38.pyc
    	new file:   morphometrics/__pycache__/morphometrics.cpython-312.pyc
    	modified:   morphometrics/__pycache__/morphometrics.cpython-38.pyc
    	modified:   morphometrics/check_waterhsed.ipynb
    	modified:   morphometrics/morphometrics.py

--- morphometrics/morphometrics.py
+++ morphometrics/morphometrics.py
@@ -182,7 +182,7 @@ def get_morphometrics(img_path):
     #     #add the new data to the morphometrics DataFrame
     #     morph_df1 = pd.concat([morph_df1,new_df], ignore_index=True)
         
-    # return morph_df2
+    return morph_df2
 
 def save_morphometrics(morph_df, output_dir, output_name):
     output_path = os.path.join(output_dir, output_name+'.xlsx')
